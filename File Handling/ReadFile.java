dc19510c1a7adfbeacdb0301cbe735c6ea1cb15e
Tuxa4Life <nikoloztuxa@gmail.com>
2023-12-15 21:05:39 +0400

    File Handling Uploaded

---
+++ File Handling/ReadFile.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File file = new File("C:\\Users\\Tuxa\\IdeaProjects\\Playground\\src\\index.html");
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                System.out.println(reader.nextLine());
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
